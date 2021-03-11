4e890f5a71a9e3ec014735dc1cc2b25a2c4b0c0f
Allyson Camitta <allyson.camitta@gmail.com>
2021-03-11 17:34:53 -0500

    add basic backend skeleton

---
+++ server/auth/index.js
@@ -0,0 +1,49 @@
+const router = require('express').Router()
+const User = require('../db/models/user')
+module.exports = router
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const {email, password} = req.body
+    const user = await User.findOne({where: {email: email}})
+    if (!user) {
+      console.log('No such user found:', email)
+      res.status(401).send('Wrong username and/or password')
+    } else if (!user.correctPassword(password)) {
+      console.log('Incorrect password for user:', email)
+      res.status(401).send('Wrong username and/or password')
+    } else {
+      req.login(user, err => (err ? next(err) : res.json(user)))
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const {firstName, lastName, email, password} = req.body
+    const user = await User.create({firstName, lastName, email, password})
+    req.login(user, err => (err ? next(err) : res.json(user)))
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists')
+    } else if (err.name === 'SequelizeValidationError') {
+      res.status(401).send(err.errors[0].message)
+    } else {
+      next(err)
+    }
+  }
+})
+
+router.post('/logout', (req, res) => {
+  req.logout()
+  req.session.destroy()
+  res.redirect('/')
+})
+
+router.get('/me', (req, res) => {
+  res.json(req.user)
+})
+
+router.use('/google', require('./google'))

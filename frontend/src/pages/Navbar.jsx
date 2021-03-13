b7cf85de587fef78ac9b061775876993129d0bf9
Annie <gjhwa11@gmail.com>
2021-03-13 17:59:07 -0500

    feature: links connected to URIs for items in the navbar

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Pollen</h1>
-            <i className="fas fa-seedling"></i>
+        <div className="nav-container">
+            <h1><Link to="/">Pollen</Link></h1>
+            <ul className="nav-links">
+                <li><Link to="/example">Example</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/login">Log In</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+            </ul>
         </div>
     )
 }

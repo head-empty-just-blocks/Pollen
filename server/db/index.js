3b62554f050a9764274cf076409cd7fdfaf59370
anahit <sathytrench@gmail.com>
2021-03-16 11:53:47 -0400

    feat(server/db): adds Project model to db, updates db/index to reflect all models

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const db = require('./db')
 
 // register models
-require('./models')
+const {User, Organization, Project} = require('./models')
 
-module.exports = db
+module.exports = {db, User, Organization, Project};

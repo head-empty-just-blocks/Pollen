75a7bd608ed94b590bc0908af6014562cfdb419f
anahit <sathytrench@gmail.com>
2021-03-17 18:06:45 -0400

    feat(server/db): creates index file in db/models where relationships are defined

---
+++ server/db/models/index.js
@@ -0,0 +1,11 @@
+const Organization = require('./Organization');
+const Project = require('./Project');
+const User = require('./User');
+
+Organization.hasMany(Project);
+Project.belongsTo(Organization);
+
+User.belongsToMany(Organization, {through: 'user-org'});
+Organization.belongsToMany(User, {through: 'user-org'});
+
+module.exports = { Organization, Project, User };

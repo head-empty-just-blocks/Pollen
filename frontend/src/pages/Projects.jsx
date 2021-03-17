8af56833a5f4a3f6bfd944880658f9c0a21f9578
Anahit Gulian <48396882+sathytrench@users.noreply.github.com>
2021-03-17 17:47:18 -0400

    Revert "map feature: connects the projects page to the google maps api"

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,37 +1,11 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import * as secrets from '../secrets'
+import React from 'react'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class Projects extends Component {
- static defaultProps = {
-    center: {
-      lat:  40.75,
-      lng: -73.99
-    },
-    zoom: 14
-  };
-
-  render() {
+const Projects = () => {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: secrets.api_key }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
-};
-
+        <div>
+            <h1>Project Map</h1>
+        </div>
+    )
+}
 
 export default Projects

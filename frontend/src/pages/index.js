a58d8f62485a57f5c673772ec7de757e548c9e11
Allyson Camitta <allyson.camitta@gmail.com>
2021-03-17 14:41:21 -0700

    feature(Map) add mapbox page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,4 @@
-/* 
+/*
 This index.js file exists solely as a central export for the pages directory
 */
 
@@ -7,5 +7,5 @@ export {default as Home} from './Home.jsx'
 export {default as Example} from './Example.jsx'
 export {default as Login} from './Login.jsx'
 export {default as Signup} from './Signup.jsx'
-export {default as Projects} from './Projects.jsx'
+export {default as Map} from './Map.jsx'
 export {default as SingleOrgView} from './SingleOrgView.jsx'

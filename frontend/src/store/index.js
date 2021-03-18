6b6238341191d93405f0f524ef7c1c7d441c4e8b
Annie <gjhwa11@gmail.com>
2021-03-18 15:38:44 -0400

    adding redux: creates store folder and files for all needed reducers

---
+++ frontend/src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import user from "./user";
+import allOrgs from "./allOrgs";
+import singleOrg from "./singleOrg";
+import allProjects from "./allProjects";
+import singleProject from "./singleProject";
+
+const reducer = combineReducers({
+  user,
+  allOrgs,
+  singleOrg,
+  allProjects,
+  singleProject,
+});
+
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
+const store = createStore(reducer, middleware);
+
+export default store;
---
+++ frontend/src/store/singleOrg.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+//action type
+const GET_SINGLE_ORG = "GET_SINGLE_ORG";
+
+//action creator
+const getSingleOrg = (org) => {
+  return {
+    type: GET_SINGLE_ORG,
+    org,
+  };
+};
+
+//thunk
+export const fetchSingleOrg = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/orgs/${id}`);
+      dispatch(getSingleOrg(data));
+    } catch (err) {
+      console.log("fetch single org thunk\n", err);
+    }
+  };
+};
+
+//initial state
+const initialState = {};
+
+//reducer
+export default function singleOrgReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_SINGLE_ORG:
+      return action.org;
+    default:
+      return state;
+  }
+}

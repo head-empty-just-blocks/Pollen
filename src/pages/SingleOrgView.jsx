1562549f74d6b34d2cc44d2d4fbf6b0bd19a754c
Annie <gjhwa11@gmail.com>
2021-03-24 15:26:32 -0400

    chore: rearranging the package.json

--- src/pages/SingleOrgView.jsx
+++ src/pages/SingleOrgView.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchSingleOrg} from '../store/singleOrg'
+import Loading from './Loading'
 
-const SingleOrgView = (props) => {
-  return (
-    <div>
-      <h1>Single Org View id:{props.match.params.id}</h1>
-    </div>
-  )
+export class SingleOrgView extends Component {
+  componentDidMount() {
+    console.log(this.props.match.params.id)
+    this.props.fetchOrg(this.props.match.params.id)
+  }
+  render() {
+    const org = this.props.org || {}
+    if (Object.keys(org).length) {
+      return (
+        <div>
+          <h1>{org.name}</h1>
+          <div>{org.description}</div>
+        </div>
+      )
+    } else return <Loading />
+  }
 }
 
-export default SingleOrgView
+const mapState = (state) => ({
+  org: state.org,
+})
+
+const mapDispatch = (dispatch) => ({
+  fetchOrg: (id) => dispatch(fetchSingleOrg(id)),
+})
+
+export default connect(mapState, mapDispatch)(SingleOrgView)

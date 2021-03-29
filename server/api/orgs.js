c04e0b0bfba2d1d6071a4a01c051b71f752df4eb
Annie <gjhwa11@gmail.com>
2021-03-29 18:25:25 -0400

    refactor: getting rid of projects api and combining it with the orgs api

--- server/api/orgs.js
+++ server/api/orgs.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {Organization} = require('../db/models')
+const {Project} = require('../db/models')
 module.exports = router
 
 // fetches all organizations
@@ -83,4 +84,54 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-router.use('/:orgId/projects', require('./projects'))
+router.get('/:orgId/projects', async (req, res, next) => {
+  try {
+    const projects = await Project.findAll({
+      attributes: [
+        'title',
+        'description',
+        'startDate',
+        'endDate',
+        'goalAmount',
+        'currentAmount',
+      ],
+      where: {
+        organizationId: req.params.orgId,
+      },
+    })
+    res.json(projects)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// create a new project with columns filled in front end
+router.post('/:orgId/projects', async (req, res, next) => {
+  try {
+    const project = await Project.findOrCreate({
+      where: {
+        title: req.body.title,
+      },
+    })
+    project.description = req.body.description
+    project.startDate = req.body.startDate
+    project.endDate = req.body.endDate
+    project.goalAmount = req.body.goalAmount
+    project.currentAmount = req.body.currentAmount
+    await project.save()
+    res.sendStatus(201)
+  } catch (error) {
+    next(error)
+  }
+})
+
+// change the currentAmount of a specified project by the amount of donation
+router.put('/:orgId/projects/:projectId/donate', async (req, res, next) => {
+  try {
+    const project = await Project.findByPk(req.params.projectId)
+    project.collectDonation(req.body.donation)
+    res.status(200).send(project)
+  } catch (error) {
+    next(error)
+  }
+})

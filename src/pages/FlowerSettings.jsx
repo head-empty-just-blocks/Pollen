da70a7c49419ddad0f7d41ba1d154e10cc510b4a
Annie <gjhwa11@gmail.com>
2021-04-01 16:44:58 -0400

    feature: flower settings now lists projects and their current amount / goal amount

--- src/pages/FlowerSettings.jsx
+++ src/pages/FlowerSettings.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchSingleOrg} from '../store/singleOrg'
+import {fetchProjects} from '../store/allProjects'
 
-const FlowerSettings = () => {
+const FlowerSettings = ({fetchOrg, user, org, projects}) => {
+  useEffect(() => {
+    fetchOrg(user.organizationId)
+    fetchProjects(user.organizationId)
+  }, [])
   return (
     <div>
       <img className="sprite" src="/assets/flower.png" />
+      <h2>{org.name}</h2>
       <Link to={'/projects/create'}>
         <button>CREATE NEW PROJECT</button>
       </Link>
+      <div>
+        {projects.map((project) => (
+          <div>
+            <div>{project.title}</div>
+            <div>
+              ${project.currentAmount} / {project.goalAmount}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default FlowerSettings
+const mapState = (state) => ({
+  user: state.user,
+  org: state.singleOrg,
+  projects: state.allProjects,
+})
+
+const mapDispatch = (dispatch) => ({
+  fetchOrg: (id) => dispatch(fetchSingleOrg(id)),
+  fetchProjects: (id) => dispatch(fetchProjects(id)),
+})
+
+export default connect(mapState, mapDispatch)(FlowerSettings)

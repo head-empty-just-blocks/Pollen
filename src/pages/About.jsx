e0915815f9dd8d543b79777d7b6c62ff2cfd33ce
Allyson Camitta <allyson.camitta@gmail.com>
2021-04-04 22:15:13 -0700

    fix UI on about us page, add footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,37 +2,37 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import {makeStyles} from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {Footer} from './index'
 
 const useStyles = makeStyles((theme) => ({
   section: {
     marginTop: theme.spacing(3),
-  },
-  toolbar: {
-    marginTop: theme.spacing(2),
-  },
-  button: {
-    marginRight: theme.spacing(1),
   },
 }))
 
 const About = () => {
   const classes = useStyles()
   return (
-    <Container className={classes.section}>
-      <Typography variant="h3" gutterBottom>
-        About Us
-      </Typography>
-      <div className={classes.section}>
-        <Typography variant="h6" gutterBottom>
-          Living in New York, we have witnessed firsthand the power in
-          harnessing community in the form of mutual aid. The city runs on the
-          kindness of strangers and we believe in the potential of capturing
-          this desire to help others and inspire actionable results. Pollen is a
-          decentralized application that promotes transparency and social impact
-          to your local community and beyond.
+    <div>
+      <CssBaseline />
+      <Container className={classes.section}>
+        <Typography variant="h3" gutterBottom>
+          About Us
         </Typography>
-      </div>
-    </Container>
+        <div className={classes.section}>
+          <Typography variant="h6" gutterBottom>
+            Living in New York, we have witnessed firsthand the power in
+            harnessing community in the form of mutual aid. The city runs on the
+            kindness of strangers and we believe in the potential of capturing
+            this desire to help others and inspire actionable results. Pollen is
+            a decentralized application that promotes transparency and social
+            impact to your local community and beyond.
+          </Typography>
+        </div>
+      </Container>
+      <Footer />
+    </div>
   )
 }
 

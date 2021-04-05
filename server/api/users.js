f72303e4bc5bf711466fb0ae1e912e296a2b7f05
anahit <sathytrench@gmail.com>
2021-04-05 12:07:34 -0400

    fix(server/api): configures user api to work with regular auth instead of oauth

--- server/api/users.js
+++ server/api/users.js
@@ -2,15 +2,36 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+const {isFlower, isYourself} = require('./securityGate')
+
+router.get('/', isFlower, async (req, res, next) => {
   try {
-    const orgs = await User.findOne({
-      attributes: ['firstName', 'lastName', 'email'],
-      where: {
-        googleId: req.body.googleId,
-      },
+    const users = await User.findAll({
+      attributes: ['id', 'email', 'name', 'isFlower'],
     })
-    res.json(orgs)
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', isYourself, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    res.send(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// toggle admin status of user
+router.put('/:id', isFlower, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id)
+    const currentFlowerStatus = user.isFlower
+    user.isFlower = !currentFlowerStatus
+    await user.save()
+    res.sendStatus(204)
   } catch (err) {
     next(err)
   }

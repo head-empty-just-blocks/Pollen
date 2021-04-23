ff15e2e5d5610ea2a1288e194032753fb3576689
Allyson Camitta <allyson.camitta@gmail.com>
2021-04-23 19:38:53 -0400

    fix footer on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,36 +15,38 @@ export const useFormControls = () => {
 const ContactForm = () => {
   const {handleFormSubmit} = useFormControls()
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        margin: 20,
-        padding: 20,
-      }}
-    >
-      <form style={{width: '50%'}} onSubmit={handleFormSubmit}>
-        <h1>Contact</h1>
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          margin: 20,
+          padding: 20,
+        }}
+      >
+        <form style={{width: '50%'}} onSubmit={handleFormSubmit}>
+          <h1>Contact</h1>
 
-        <FormControl margin="normal" fullWidth>
-          <InputLabel htmlFor="name">Name</InputLabel>
-          <Input id="name" type="text" />
-        </FormControl>
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input id="name" type="text" />
+          </FormControl>
 
-        <FormControl margin="normal" fullWidth>
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <Input id="email" type="email" />
-        </FormControl>
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" type="email" />
+          </FormControl>
 
-        <FormControl margin="normal" fullWidth>
-          <InputLabel htmlFor="email">Message</InputLabel>
-          <Input id="email" multiline rows={10} />
-        </FormControl>
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="email">Message</InputLabel>
+            <Input id="email" multiline rows={10} />
+          </FormControl>
 
-        <Button variant="contained" color="primary" size="medium">
-          Send
-        </Button>
-      </form>
+          <Button variant="contained" color="primary" size="medium">
+            Send
+          </Button>
+        </form>
+      </div>
       <Footer />
     </div>
   )
